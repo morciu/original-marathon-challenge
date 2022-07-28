48fb16ef520075541d1afa94aa555a94c7fab086
morciu <marianpralea@gmail.com>
2022-07-28 18:18:06 +0300

    MainMenu class now checks for user input.

---
+++ Solution1/ClassLibrary1/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public static class MainMenu
+    {
+        public const string title = "Run The Original Marathon!";
+        public const string introMsg = "\nDuring the Battle of Marathon, according to Herodotus, an Athenian runner named Pheidippides was sent to run from Athens to Sparta to ask for assistance before the battle. \nHe ran a distance of over 225 kilometers (140 miles), arriving in Sparta the day after he left.\nRecreate this distance in real life, at your own pace and in as many runs as you can.";
+        public const string mainMenuOptions = "\nOptions:\n1. Create New User\n2. Log in with existing User\n0. Exit\n";
+
+        public static void DisplayIntroMenu()
+        {
+            Console.WriteLine(title);
+            Console.WriteLine(introMsg);
+            Console.WriteLine(mainMenuOptions);
+        }
+
+        public static string GetInput()
+        {
+            while (true)
+            {
+                string? input = Console.ReadLine();
+                if (input == "1")
+                {
+                    return input;
+                }
+                if (input == "2")
+                {
+                    return input;
+                }
+                if (input == "0")
+                {
+                    return input;
+                }
+            }
+        }
+    }
+}
--- Solution1/ConsoleApp1/Program.cs
+++ Solution1/ConsoleApp1/Program.cs
@@ -1,8 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Run The Original Marathon!");
-Console.WriteLine("\nDuring the Battle of Marathon, according to Herodotus, an Athenian runner named Pheidippides was sent to run from Athens to Sparta to ask for assistance before the battle. \nHe ran a distance of over 225 kilometers (140 miles), arriving in Sparta the day after he left.");
-Console.WriteLine("\nRecreate this distance in real life, at your own pace and in as many runs as you can.");
-Console.WriteLine("\nOptions:");
-Console.WriteLine("1. Create New User");
-Console.WriteLine("2. Log in with existing User");
-Console.WriteLine("0. Exit\n");
+﻿using ClassLibrary1;
+
+// Show Main Menu
+MainMenu.DisplayIntroMenu();
+
+// Check user input
+Console.WriteLine(MainMenu.GetInput());
