803de96a20999b15040b2e87f97c3441f3558e17
morciu <marianpralea@gmail.com>
2022-08-12 12:23:18 +0300

    Cleaned up old commented out code

--- Solution1/ConsoleApp1/Program.cs
+++ Solution1/ConsoleApp1/Program.cs
@@ -10,17 +10,6 @@ using ConsolePresentation.Menus;
 
 
 
-// Building Container
-var diContainer = new ServiceCollection()
-    .AddScoped<IUserRepository, InMemoryUserRepository>()
-    .AddScoped<IActivityRepository, InMemoryActivityRepository>()
-    .AddMediatR(typeof(IUserRepository))
-    .AddMediatR(typeof(IActivityRepository))
-    .BuildServiceProvider();
-
-// Get mediator
-var mediator = diContainer.GetRequiredService<IMediator>();
-
 AppState app = new AppState();
 
 while (true)
@@ -28,54 +17,3 @@ while (true)
     app.RunApp();
 }
 
-
-
-/*// Create a new user through the mediator
-var user = await mediator.Send(new CreateUserCommand
-{
-    Id = 5,
-    FirstName = "SomeFirstName",
-    LastName = "SomeLastName",
-    UserName = "SomeUserName",
-    Password = "SomePassWord"
-});
-
-Console.WriteLine(user.Id);
-Console.WriteLine(user.FirstName);
-Console.WriteLine(user.UserName);
-Console.WriteLine(user.LastName);
-
-// Get first user through mediator
-var firstUser = await mediator.Send(new GetUserQueryCommand
-{
-    Id = 1
-});
-
-Console.WriteLine($"\nFirst User: {firstUser.UserName}\n");*/
-
-
-/*// Set up main menu
-IMenu currentMenu = new MainMenu();
-
-// Current user Id
-int currentUserId;
-
-// Set up User Input
-string userInput;
-
-// Load registered users
-
-// Testing Input
-while (true)
-{
-    // Display current menu
-    currentMenu.DisplayMenu();
-    // Get user input
-    userInput = currentMenu.GetInput();
-    // Process user input
-    currentMenu.ProcessInput(userInput);
-    // Switch menu
-    currentMenu.SwitchMenu(userInput, ref currentMenu);
-    // Update/Change current menu
-
-}*/
