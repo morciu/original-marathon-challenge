f2ae47589465868e6779a0c232cf272e16f7b962
morciu <marianpralea@gmail.com>
2022-09-03 13:38:54 +0300

    Created test for GetAll action

--- Solution1/Test/UsersControllerFixture.cs
+++ Solution1/Test/UsersControllerFixture.cs
@@ -24,9 +24,9 @@ namespace Test
                 .Setup(m => m.Send(It.IsAny<GetUserByIdQueryCommand>(), It.IsAny<CancellationToken>()))
                 .Verifiable();
             _mockHelper
-                .Setup(h => h.GetControllerName(It.IsAny<UsersController>())).Returns("TestControllerName");
+                .Setup(h => h.GetControllerName(It.IsAny<UsersController>())).Returns("UsersControllerTest");
             _mockHelper
-                .Setup(h => h.GetActionName(It.IsAny<UsersController>())).Returns("TestActionName");
+                .Setup(h => h.GetActionName(It.IsAny<UsersController>())).Returns("GetUserByIdTest");
 
             var controller = new UsersController(_mockMediator.Object, _mockMapper.Object, _mockLogger.Object, _mockHelper.Object);
 
@@ -36,6 +36,27 @@ namespace Test
 
             // Assert
             _mockMediator.Verify(m => m.Send(It.IsAny<GetUserByIdQueryCommand>(), It.IsAny<CancellationToken>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task Get_All_GetAllIsCalled()
+        {
+            // Arrange
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetAllUsers>(), It.IsAny<CancellationToken>()))
+                .Verifiable();
+            _mockHelper
+                .Setup(h => h.GetControllerName(It.IsAny<UsersController>())).Returns("UsersControllerTest");
+            _mockHelper
+                .Setup(h => h.GetActionName(It.IsAny<UsersController>())).Returns("GetAllTest");
+
+            var controller = new UsersController(_mockMediator.Object, _mockMapper.Object, _mockLogger.Object, _mockHelper.Object);
+
+            // Act
+            await controller.GetAll();
+
+            // Assert
+            _mockMediator.Verify(m => m.Send(It.IsAny<GetAllUsers>(), It.IsAny<CancellationToken>()), Times.Once());
         }
     }
 }
