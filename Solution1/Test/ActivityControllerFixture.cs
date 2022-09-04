e63039301aae9acf66f132806a771e5c31eacce4
morciu <marianpralea@gmail.com>
2022-09-04 14:04:37 +0300

    Created test for CreateActivity.

--- Solution1/Test/ActivityControllerFixture.cs
+++ Solution1/Test/ActivityControllerFixture.cs
@@ -1,10 +1,12 @@
-﻿using Application.Activities.Queries;
+﻿using Application.Activities.Commands;
+using Application.Activities.Queries;
 using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using Moq;
 using WebAPI.Controllers;
 using WebAPI.ControllersHelpers;
+using WebAPI.Dto;
 
 namespace Test
 {
@@ -76,6 +78,35 @@ namespace Test
 
             // Assert
             _mockMediator.Verify(m => m.Send(It.IsAny<GetAllUserActivitiesQuery>(), It.IsAny<CancellationToken>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task Create_Activity_CreateActivityIsCalled()
+        {
+            // Arrange
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<CreateActivityCommand>(), It.IsAny<CancellationToken>()))
+                .Verifiable();
+            _mockHelper
+                .Setup(h => h.GetControllerName(It.IsAny<ActivityController>())).Returns("ActivityControllerTest");
+            _mockHelper
+                .Setup(h => h.GetActionName(It.IsAny<ActivityController>())).Returns("CreateActivityTest");
+
+            var controller = new ActivityController(_mockMediator.Object, _mockMapper.Object, _mockLogger.Object, _mockHelper.Object);
+
+            var dto = new ActivityPutPostDto()
+            {
+                UserId = 1,
+                Distance = 5.56m,
+                Date = DateTime.Now,
+                Duration = new TimeSpan(0, 32, 23),
+            };
+
+            // Act
+            await controller.CreateActivity(dto);
+
+            // Assert
+            _mockMediator.Verify(m => m.Send(It.IsAny<CreateActivityCommand>(), It.IsAny<CancellationToken>()), Times.Once());
         }
     }
 }
