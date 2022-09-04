b73fa535a987a549881212846a66179f6d182667
morciu <marianpralea@gmail.com>
2022-09-04 13:56:07 +0300

    Created test for GetAllUserActivities

--- Solution1/Test/ActivityControllerFixture.cs
+++ Solution1/Test/ActivityControllerFixture.cs
@@ -56,5 +56,26 @@ namespace Test
             // Assert
             _mockMediator.Verify(m => m.Send(It.IsAny<GetActivityByIdQuery>(), It.IsAny<CancellationToken>()), Times.Once());
         }
+
+        [Fact]
+        public async Task Get_All_User_Activities_GetAllUserActivitiesIsCalled()
+        {
+            // Arrange
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetAllUserActivitiesQuery>(), It.IsAny<CancellationToken>()))
+                .Verifiable();
+            _mockHelper
+                .Setup(h => h.GetControllerName(It.IsAny<ActivityController>())).Returns("ActivityControllerTest");
+            _mockHelper
+                .Setup(h => h.GetActionName(It.IsAny<ActivityController>())).Returns("GetAllUserActivitiesTest");
+
+            var controller = new ActivityController(_mockMediator.Object, _mockMapper.Object, _mockLogger.Object, _mockHelper.Object);
+
+            // Act
+            await controller.GetAllUserActivities(5);
+
+            // Assert
+            _mockMediator.Verify(m => m.Send(It.IsAny<GetAllUserActivitiesQuery>(), It.IsAny<CancellationToken>()), Times.Once());
+        }
     }
 }
