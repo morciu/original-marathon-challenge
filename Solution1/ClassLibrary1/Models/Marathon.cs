5514129a43088942479e2b8f5216114e3ef2cc56
morciu <marianpralea@gmail.com>
2022-09-16 12:57:25 +0300

    Created test for Activity.CalculatePace()

--- Solution1/ClassLibrary1/Models/Marathon.cs
+++ Solution1/ClassLibrary1/Models/Marathon.cs
@@ -11,15 +11,5 @@ namespace Domain.Models
         public ICollection<User> Members { get; set; }
         public int Id { get; set; }
         public DateTime StartDate { get; set; }
-
-        public decimal CalculateTotalDistaceForMember(User user)
-        {
-            decimal result = 0;
-            foreach(var activity in user.Activities)
-            {
-                result += activity.Distance;
-            }
-            return result;
-        }
     }
 }
---
+++ Solution1/Test/ActivityFixture.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class ActivityFixture
+    {
+        private readonly Mock<User> _mockUser = new();
+        [Fact]
+        public void CalculatePace_CalculatesAndReturnsCorrectPace()
+        {
+            // Arrange
+            Activity activity = new Activity { Id = 1, Duration = TimeSpan.Parse("00:55:51"), Date = DateTime.Now, User = _mockUser.Object, Distance = 10.98m };
+
+            // Act
+            var pace = activity.CalculatePace(activity.Distance, activity.Duration);
+
+            // Assert
+            Assert.Equal(pace, TimeSpan.Parse("00:05:05"));
+        }
+    }
+}
