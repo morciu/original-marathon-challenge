0f61e4e0d4e3dbf813e5d6ccc13ee745a81788c4
morciu <marianpralea@gmail.com>
2022-09-18 19:43:33 +0300

    fixed browser error handlling onClick event for button component

--- reactapp/src/components/Buttons/MainButton.jsx
+++ reactapp/src/components/Buttons/MainButton.jsx
@@ -13,7 +13,7 @@ const Button = (props) => {
 
     return(
         <>
-            <button style={style} type="submit"  onClick={props.clickEvent}>Log In</button>
+            <button style={style} type="submit"  onClick={() => props.clickEvent}>Log In</button>
         </>
     );
 };
