8bb1faf9a58a05ec6167a9c02a9a853a016cb24e
morciu <marianpralea@gmail.com>
2022-09-21 19:28:31 +0300

    Installed MUI icon pack and used an icon for the register button.

--- reactapp/src/pages/Login/Login.jsx
+++ reactapp/src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styles from "./Login.module.css";
 import Button from "@mui/material/Button";
+import AppRegistrationRoundedIcon from '@mui/icons-material/AppRegistrationRounded';
 import TextInput from "../../components/Inputs/TextInput";
 
 const Login = (props) => {
@@ -12,7 +13,8 @@ const Login = (props) => {
                 <TextInput label="Password" type="password"/>
                 
                 <Button variant="contained">Log In</Button>
-                <Button href="/register">Register</Button>
+                <Button variant="outlined" href="/register" startIcon={<AppRegistrationRoundedIcon />}>
+                    Register</Button>
             </div>
         </>
     )
