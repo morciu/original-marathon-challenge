77b455fefb155fc10b0cc0d2129ebb88b19e9204
morciu <marianpralea@gmail.com>
2022-09-22 20:56:38 +0300

    React - Getting Register form data in component state

--- reactapp/src/pages/Register/Register.jsx
+++ reactapp/src/pages/Register/Register.jsx
@@ -1,20 +1,39 @@
-import React from "react";
-import styles from "./Register.module.css";
+import React, { useEffect, useState } from "react";
+import styles from "./Register.module.css"
 import Button from "@mui/material/Button";
 import TextInput from "../../components/Inputs/TextInput";
 import { TextField } from "@mui/material";
 
 const Register = (props) =>{
 
-    return(
-        <div className={styles.registerContainer}>
-            <TextField label="First Name" variant="outlined" />
-            <TextField label="Last Name" variant="outlined" />
-            <TextField label="User Name" variant="outlined" />
-            <TextField type="password" label="Password" variant="outlined" />
+    const [registerState, setRegisterState ] = useState({
+        "firstName": "",
+        "lastName": "",
+        "userName": "",
+        "password": "",
+    });
 
-            <Button variant="contained">Register</Button>
-        </div>
+    return(
+        <form className={styles.registerContainer}
+            onSubmit={(e) => {
+                e.preventDefault();
+                console.log(registerState);
+            }}>
+            <TextField 
+                onChange={(e) => {setRegisterState({...registerState, "firstName": e.target.value})}}
+                label="First Name" variant="outlined" />
+            <TextField 
+                onChange={(e) => {setRegisterState({...registerState, "lastName": e.target.value})}}
+                label="Last Name" variant="outlined" />
+            <TextField 
+                onChange={(e) => {setRegisterState({...registerState, "userName": e.target.value})}}
+                label="User Name" variant="outlined" />
+            <TextField 
+                onChange={(e) => {setRegisterState({...registerState, "password": e.target.value})}}
+                type="password" label="Password" variant="outlined" />
+
+            <Button type="submit" variant="contained">Register</Button>
+        </form>
     );
 };
 
