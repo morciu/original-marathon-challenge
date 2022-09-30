ba23ad4897ba3b54c1180e39e2a1894b4e04465a
morciu <marianpralea@gmail.com>
2022-09-30 15:13:14 +0300

    styled user cards and created modal popups for each one

--- reactapp/src/pages/Marathon/Marathon.jsx
+++ reactapp/src/pages/Marathon/Marathon.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import { React, useState } from "react";
 import { useParams } from "react-router-dom";
 import useFetchData from "../../hooks/useFetchData";
 import { useContext } from "react";
 import { UserContext } from "../../hooks/UserContext";
 
-import { Stack, Card, CardContent, Link, Typography } from "@mui/material";
+import { Stack, Card, CardContent, Link, Typography, makeStyles, CardHeader, Avatar, IconButton, Modal } from "@mui/material";
 import axios from "axios";
+import { Favorite } from "@mui/icons-material";
+import { Box } from "@mui/system";
 
 const Marathon = () => {
     const {user} = useContext(UserContext);
@@ -33,26 +35,79 @@ const Marathon = () => {
         });
         return response.data;
     }
+
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      };
+      
+      const [modalObject, setmodalObject] = useState({
+            id: null,
+            userName: null,
+            totalDistance: null,
+            totalTime: null,
+            averagePace: null
+        });
+
+      const[openModal, setOpenModal] = useState(false);
+      const handleOpenModal = (item) => {
+        setmodalObject(item);
+        setOpenModal(true);
+    }
+      const handleCloseModal = () => setOpenModal(false);
     
 
     // Display Results
     return(
         <>
-        <Typography variant="h5">Personal Progress: {}% </Typography>
-        <Stack spacing={2}>
-                {loading && <p>Loading...</p>}
-                {error && <p>{error.message}</p>}
-                {data && data.map((item) => (
-                    <Stack item key={item.id}>
-                        <Card>
-                            <CardContent component={Link} to={`/user/${item.id}`}>
-                                <Typography color={"black"} variant="h5">Username: {item.userName}</Typography>
-                                <Typography color={"black"} variant="h5">Total Distance: {item.totalDistance} km</Typography>
-                            </CardContent>
+            <Typography variant="h5">Personal Progress: {}% </Typography>
+            
+            <Modal open={openModal}
+                onClose={handleCloseModal}
+                aria-labelledby={modalObject.id}>
+                <Box sx={style}>
+                    <Typography variant="h6" component="h2">
+                        {modalObject.userName}
+                    </Typography>
+                    <Typography sx={{ mt: 2 }}>
+                        Total Distance: {modalObject.totalDistance} km
+                    </Typography>
+                    <Typography sx={{ mt: 2 }}>
+                        Total Time spent running: {modalObject.totalTime}
+                    </Typography>
+                    <Typography sx={{ mt: 2 }}>
+                        Average Pace: {modalObject.averagePace != null && modalObject.averagePace.substring(3)}
+                    </Typography>
+                </Box>
+            </Modal>
+
+            <Stack spacing={2}>
+                    {loading && <p>Loading...</p>}
+                    {error && <p>{error.message}</p>}
+                    {data && data.map((item) => (
+                        <Card onClick={() => { handleOpenModal(item) }}>
+                            <CardHeader
+                                avatar={
+                                    <Avatar aria-label="recipe" >
+                                        {item.userName != null && item.userName.substring(0,1)}
+                                    </Avatar>
+                                }
+                                action={
+                                    <IconButton aria-label="settings"><Favorite /></IconButton>
+                                }
+                                title={item.userName}
+                                subheader={item.totalDistance+" km"}
+                                />
                         </Card>
-                    </Stack>
-            ))}
-        </Stack>
+                    ))}
+            </Stack>
         </>
     );
 };
