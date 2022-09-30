df4990b54191ad4f04de28b6e3e37aac821661cd
morciu <marianpralea@gmail.com>
2022-09-30 15:42:07 +0300

    Created UserModal component in react

---
+++ reactapp/src/components/UserModal/UserModal.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Modal, Box, Typography } from "@mui/material";
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+const UserModal = (props) => {
+    return(
+        <Modal open={props.open}
+                onClose={props.handleCloseModal}
+                aria-labelledby={props.modalObject.id}>
+            <Box sx={style}>
+                <Typography variant="h6" component="h2">
+                    {props.modalObject.userName}
+                </Typography>
+                <Typography sx={{ mt: 2 }}>
+                    Total Distance: {props.modalObject.totalDistance} km
+                </Typography>
+                <Typography sx={{ mt: 2 }}>
+                    Total Time spent running: {props.modalObject.totalTime}
+                </Typography>
+                <Typography sx={{ mt: 2 }}>
+                    Average Pace: {props.modalObject.averagePace != null && props.modalObject.averagePace.substring(3)}
+                </Typography>
+            </Box>
+        </Modal>
+    );
+};
+
+export default UserModal;
--- reactapp/src/pages/Marathon/Marathon.jsx
+++ reactapp/src/pages/Marathon/Marathon.jsx
@@ -8,6 +8,7 @@ import { Stack, Card, CardContent, Link, Typography, makeStyles, CardHeader, Ava
 import axios from "axios";
 import { Favorite } from "@mui/icons-material";
 import { Box } from "@mui/system";
+import UserModal from "../../components/UserModal/UserModal";
 
 const Marathon = () => {
     const {user} = useContext(UserContext);
@@ -34,80 +35,52 @@ const Marathon = () => {
             },
         });
         return response.data;
-    }
-
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
-      };
+    };
       
-      const [modalObject, setmodalObject] = useState({
-            id: null,
-            userName: null,
-            totalDistance: null,
-            totalTime: null,
-            averagePace: null
-        });
+    const [modalObject, setmodalObject] = useState({
+        id: null,
+        userName: null,
+        totalDistance: null,
+        totalTime: null,
+        averagePace: null
+    });
 
-      const[openModal, setOpenModal] = useState(false);
-      const handleOpenModal = (item) => {
-        setmodalObject(item);
-        setOpenModal(true);
+    const[openModal, setOpenModal] = useState(false);
+    const handleOpenModal = (item) => {
+    setmodalObject(item);
+    setOpenModal(true);
     }
-      const handleCloseModal = () => setOpenModal(false);
-    
+    const handleCloseModal = () => setOpenModal(false);
 
     // Display Results
     return(
         <>
-            <Typography variant="h5">Personal Progress: {}% </Typography>
-            
-            <Modal open={openModal}
-                onClose={handleCloseModal}
-                aria-labelledby={modalObject.id}>
-                <Box sx={style}>
-                    <Typography variant="h6" component="h2">
-                        {modalObject.userName}
-                    </Typography>
-                    <Typography sx={{ mt: 2 }}>
-                        Total Distance: {modalObject.totalDistance} km
-                    </Typography>
-                    <Typography sx={{ mt: 2 }}>
-                        Total Time spent running: {modalObject.totalTime}
-                    </Typography>
-                    <Typography sx={{ mt: 2 }}>
-                        Average Pace: {modalObject.averagePace != null && modalObject.averagePace.substring(3)}
-                    </Typography>
-                </Box>
-            </Modal>
+        <Typography variant="h5">Personal Progress: {}% </Typography>
+        
+        <UserModal open={openModal}
+            modalObject={modalObject}
+            handleCloseModal={handleCloseModal} />
 
-            <Stack spacing={2}>
-                    {loading && <p>Loading...</p>}
-                    {error && <p>{error.message}</p>}
-                    {data && data.map((item) => (
-                        <Card onClick={() => { handleOpenModal(item) }}>
-                            <CardHeader
-                                avatar={
-                                    <Avatar aria-label="recipe" >
-                                        {item.userName != null && item.userName.substring(0,1)}
-                                    </Avatar>
-                                }
-                                action={
-                                    <IconButton aria-label="settings"><Favorite /></IconButton>
-                                }
-                                title={item.userName}
-                                subheader={item.totalDistance+" km"}
-                                />
-                        </Card>
-                    ))}
-            </Stack>
+        <Stack spacing={2}>
+                {loading && <p>Loading...</p>}
+                {error && <p>{error.message}</p>}
+                {data && data.map((item) => (
+                    <Card key={item.id} onClick={() => { handleOpenModal(item) }}>
+                        <CardHeader
+                            avatar={
+                                <Avatar aria-label="recipe" >
+                                    {item.userName != null && item.userName.substring(0,1)}
+                                </Avatar>
+                            }
+                            action={
+                                <IconButton aria-label="settings"><Favorite /></IconButton>
+                            }
+                            title={item.userName}
+                            subheader={item.totalDistance+" km"}
+                            />
+                    </Card>
+                ))}
+        </Stack>
         </>
     );
 };
