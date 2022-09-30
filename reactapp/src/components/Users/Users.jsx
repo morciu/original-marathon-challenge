9694fb49e1350527a447bcbbaae5321f82d6d424
morciu <marianpralea@gmail.com>
2022-09-30 18:05:59 +0300

    updated user cards on global list

--- reactapp/src/components/Users/Users.jsx
+++ reactapp/src/components/Users/Users.jsx
@@ -1,7 +1,9 @@
-import { Grid, Card, CardContent, Typography, Stack } from "@mui/material";
+import { Favorite } from "@mui/icons-material";
+import { Grid, Card, CardContent, Typography, Stack, CardHeader, Avatar, IconButton } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import useFetchData from "../../hooks/useFetchData";
+import UserModal from "../UserModal/UserModal";
 import styles from "./Users.module.css"
 
 const Users = () => {
@@ -15,24 +17,51 @@ const Users = () => {
     };
 
     const {data, loading, error} = useFetchData(requestConfig);
+
+    const [modalObject, setmodalObject] = useState({
+        id: null,
+        userName: null,
+        totalDistance: null,
+        totalTime: null,
+        averagePace: null
+    });
+
+    const[openModal, setOpenModal] = useState(false);
+    const handleOpenModal = (item) => {
+    setmodalObject(item);
+    setOpenModal(true);
+    }
+    const handleCloseModal = () => setOpenModal(false);
     
 
     // Display Results
     return(
+        <>
+        <UserModal open={openModal}
+            modalObject={modalObject}
+            handleCloseModal={handleCloseModal} />
+
         <Stack spacing={2}>
-                {loading && <p>Loading...</p>}
-                {error && <p>{error.message}</p>}
-                {data && data.map((item) => (
-                <Stack item key={item.id}>
-                    <Card>
-                        <CardContent component={Link} to={`/user/${item.id}`}>
-                            <Typography color={"black"} variant="h5">Username: {item.userName}</Typography>
-                            <Typography color={"black"} variant="h5">Total Distance: {item.totalDistance} km</Typography>
-                        </CardContent>
-                    </Card>
-                </Stack>
+            {loading && <p>Loading...</p>}
+            {error && <p>{error.message}</p>}
+            {data && data.map((item) => (
+                <Card key={item.id} onClick={() => { handleOpenModal(item) }}>
+                    <CardHeader
+                        avatar={
+                            <Avatar aria-label="recipe" >
+                                {item.userName != null && item.userName.substring(0,1)}
+                            </Avatar>
+                        }
+                        action={
+                            <IconButton aria-label="settings"><Favorite /></IconButton>
+                        }
+                        title={item.userName}
+                        subheader={item.totalDistance+" km"}
+                        />
+                </Card>
             ))}
         </Stack>
+        </>
     );
 };
 
