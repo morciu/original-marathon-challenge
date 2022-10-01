489cf9122aad22d55be1eba66b355a02481d22ef
morciu <marianpralea@gmail.com>
2022-10-01 16:59:57 +0300

    Created invitation model

---
+++ RunningChallenge/src/Domain/Models/Invitation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Invitation
+    {
+        public bool IsActive { get; set; } = false;
+        public User? Sender { get; set; }
+        public User? Receiver { get; set; }
+        public Marathon? Marathon { get; set; }
+
+        public Invitation(User? sender, User? receiver, Marathon? marathon)
+        {
+            IsActive = true;
+            Sender = sender;
+            Receiver = receiver;
+            Marathon = marathon;
+        }
+    }
+}
