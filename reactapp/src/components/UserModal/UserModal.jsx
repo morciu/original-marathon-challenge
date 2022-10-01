8897da4b9b098b65889f11d664eae943059d8260
morciu <marianpralea@gmail.com>
2022-10-01 16:47:46 +0300

    fixed run activity child modal bug... again...

--- reactapp/src/components/UserModal/UserModal.jsx
+++ reactapp/src/components/UserModal/UserModal.jsx
@@ -32,7 +32,7 @@ const UserModal = (props) => {
                 <Typography sx={{ mt: 2 }}>
                     Average Pace: {props.modalObject.averagePace != null && props.modalObject.averagePace.substring(3)}
                 </Typography>
-                {!!props.modalObject?.activities.length && 
+                {!!props.modalObject.activities?.length && 
                     <Button variant="contained" onClick={props.action}>Check Runs</Button>}
             </Box>
         </Modal>
