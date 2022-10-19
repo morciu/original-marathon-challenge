ca48328a845bfb2eab5faed9ffcd88f79981aacf
morciu <marianpralea@gmail.com>
2022-10-19 13:18:51 +0300

    Message rendered when no trophies on page

--- reactapp/src/pages/Medals/Medals.jsx
+++ reactapp/src/pages/Medals/Medals.jsx
@@ -32,27 +32,33 @@ const Medals = () => {
     // Fetch medals
     const {data, loading, error} = useFetchData(requestConfig);
 
-    return(
-        <>
-        <Backdrop
-            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-            open={loading}>
-            <CircularProgress color="inherit" />
-        </Backdrop>
-
-        <Stack spacing={2}>
-            {data.data && data.data.map((item) => (
-                <MedalCard key={item.id} item={item} />
-            ))}
-            
-        </Stack>
-
-        <Pagination 
-            count={data.totalPages}
-            onChange={selectPage}>
-        </Pagination>
-        </>
-    );
+    if(data.data?.length < 1) { 
+        return(
+        <Typography variant="h6">You haven't earned any trophies yet!</Typography>
+        )}
+    else {
+        return(
+            <>
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={loading}>
+                <CircularProgress color="inherit" />
+            </Backdrop>
+    
+            <Stack spacing={2}>
+                {data.data && data.data.map((item) => (
+                    <MedalCard key={item.id} item={item} />
+                ))}
+                
+            </Stack>
+    
+            <Pagination 
+                count={data.totalPages}
+                onChange={selectPage}>
+            </Pagination>
+            </>
+        );
+    }
 };
 
 export default Medals;
